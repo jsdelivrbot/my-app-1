1fe59c3667892d3681bffbad64f2ee77e0ecd38c
gsaray <gsaray@mobify.com>
2017-05-10 16:01:15 -0700

    start fresh

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,7 @@
 import { combineReducers } from 'redux';
-import postReducer from './reducer_post'
-import {reducer as formReducer} from 'redux-form'
 
 const rootReducer = combineReducers({
-  posts: postReducer,
-  form: formReducer
+  state: (state = {}) => state
 });
 
 export default rootReducer;
